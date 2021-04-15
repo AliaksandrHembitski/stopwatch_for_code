01efa534029bc78e573aefc4a711d0c0d8f84894
Aliaksandr Hembitski <ahembitski@gmail.com>
2021-04-15 12:49:28 +0300

    'stopwatch_for_code'

--- stopwatch_for_code.py
+++ stopwatch_for_code.py
@@ -10,6 +10,12 @@ class Timing:
         # запускаем декорируемую фунцию file_name
         self.file_name = file_name()
 
+        # определяем имя файла
+        self.name = self.file_name[0]
+
+        # определяем кол-во повторений кода
+        self.repeat = self.file_name[1]
+
         # запуск контекстного менеджера
         with self:
             pass
@@ -18,17 +24,17 @@ class Timing:
     def __enter__(self):
 
         # открывем файл с именем, полученым из фунции file_name
-        self.file = open(self.file_name, 'r')
+        self.file = open(self.name, 'r')
 
         # итарируем файл self.file и переобразуем в код
         self.code_test = ''.join([string for string in self.file])
 
         # используя timeit.repeat, фиксируем время выполнения кода, производим расчет среднего времени выполнения
         # repeat кол-во прогонов кода
-        self.timeming = sum(timeit.repeat(self.code_test, repeat=5)) / 5
+        self.timeming = sum(timeit.repeat(self.code_test, repeat=self.repeat)) / self.repeat
 
         # вывод результата
-        print(f'Среднее время выполнения кода при 5 разовом запуске:{self.timeming:.5f}')
+        print(f'Среднее время выполнения кода при {self.repeat} запусках:{self.timeming:.5f}')
 
     # окончание работы контекстного менеджера
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -43,9 +49,15 @@ def file_name():
 
     # запрос имени тестируемого файла
     name = input('Введите название тестируемого файла: ')
-    
+
+    # запрос кол-ва повторений
+    repeat = int(input('Введите кол-во повторений: '))
+
+    # объединяем запрашиваемые данные в кортеж
+    params = (name, repeat)
+
     # возвращение результата работы фунции
-    return name
+    return params
 
 # with Timing(file_name) as file:
 #     pass
